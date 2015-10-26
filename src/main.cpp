1aaa57e9ef77b0bc611efb20b14e0f4022264d40
Patric Schmitz <bzk0711@aol.com>
2015-10-26 06:54:28 +0100

    Some stylistic changes:
    - order includes from generic to specific
    - wrap comments at 80 chars
    - space before { on same line
    - wrap parameters/conditions to increase readability (key_callback)
    - wrap single line if statement to increase readability
    - use auto only in places where type is obvious from context (iterators, casts..)
    - more newlines to seperate functional blocks

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,29 @@
-//#define GLFW_INCLUDE_GLCOREARB for modern OpenGL usage; example is in old GL
-#include <GLFW/glfw3.h>
-#include <cstdio>
 #include <cstdlib>
+#include <cstdio>
 
-static void error_callback(int error, const char * description){
-  fputs(description,stderr);
+//#define GLFW_INCLUDE_GLCOREARB for modern OpenGL usage; example is
+//#in old GL
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char * description) {
+  fputs(description, stderr);
 };
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+static void key_callback(
+  GLFWwindow* window,
+  int key, int scancode, int action, int mods) {
+  if(key == GLFW_KEY_ESCAPE &&
+     action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
 int main(){
-
   glfwSetErrorCallback(error_callback); 
 
-  if(!glfwInit()) exit(EXIT_FAILURE);
+  if(!glfwInit())
+    exit(EXIT_FAILURE);
 
-  auto window = glfwCreateWindow(640,480,"Project",NULL,NULL);
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Project", NULL, NULL);
 
   if(window == nullptr){
     glfwTerminate();
@@ -29,15 +34,16 @@ int main(){
   glfwSwapInterval(1);
   glfwSetKeyCallback(window, key_callback);
 
-
-  
-  while (!glfwWindowShouldClose(window)) {
-    /*openGL functions taken from http://igorbarbosa.com/articles/how-to-use-opengl-freeglut-and-cmake/ and adapted to a very simple red triangle
+  while(!glfwWindowShouldClose(window)) {
+    /*openGL functions taken from
+     * http://igorbarbosa.com/articles/how-to-use-opengl-freeglut-and-cmake/
+     * and adapted to a very simple red triangle
      */
     
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
+
     glClear(GL_COLOR_BUFFER_BIT);
     
     glBegin(GL_TRIANGLES);
@@ -46,9 +52,11 @@ int main(){
     glVertex3f(0.6f, -0.4f, 0.f);
     glVertex3f(0.f, 0.6f, 0.f);
     glEnd();
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+  
   glfwDestroyWindow(window);  
   
   return 0;
