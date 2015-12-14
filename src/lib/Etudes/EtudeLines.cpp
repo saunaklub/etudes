a71b1ff525cad6bfe748971bb0b46914228620b8
Patric Schmitz <flavi0@openmailbox.org>
2015-12-14 20:15:52 +0100

    change default color base value

--- src/lib/Etudes/EtudeLines.cpp
+++ src/lib/Etudes/EtudeLines.cpp
@@ -50,7 +50,7 @@ namespace etudes {
         registerInput("width/omega",     {0.0f});
         registerInput("width/lambda",    {0.0f});
 
-        registerInput("color/base",      {0.5f, 0.5f, 0.5f});
+        registerInput("color/base",      {1.0f, 1.0f, 1.0f});
         registerInput("color/amplitude", {0.0f, 0.0f, 0.0f});
         registerInput("color/omega",     {0.0f});
         registerInput("color/lambda",    {0.0f});
