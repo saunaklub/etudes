f9b6c7721fb82b0e1ed6cb3fce2f2780d80c3963
Claudio Cabral <cabral1349@gmail.com>
2015-12-23 14:45:10 +0100

    changed error type in ReceiverRegistry and more
    Also changed strings to string literals

--- src/lib/Receiver/ReceiverRegistry.cpp
+++ src/lib/Receiver/ReceiverRegistry.cpp
@@ -9,7 +9,7 @@ namespace etudes {
         std::shared_ptr<Receiver> receiver) {
 
         if(registry.find("name") != registry.end())
-            throw std::logic_error(
+            throw std::invalid_argument(
                 std::string("ReceiverRegistry::registerReceiver: ") +
                 name + " already registered");
 
@@ -19,9 +19,9 @@ namespace etudes {
     Receiver &ReceiverRegistry::getReceiver(std::string name) {
         auto pair = registry.find(name);
         if(pair == registry.end())
-            throw std::logic_error(
-                std::string("ReceiverRegistry::registerReceiver: ") +
-                name + " not registered");
+            throw std::invalid_argument(
+            "ReceiverRegistry::registerReceiver: "s +
+                name + " not registered"s);
         return *(pair->second);
     }
 
