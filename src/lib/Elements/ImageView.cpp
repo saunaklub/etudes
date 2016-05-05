26102039850d9ac648e375205f2adbcf10f80206
Patric Schmitz <flavi0@openmailbox.org>
2016-05-05 15:46:12 +0200

    update license headers

--- src/lib/Elements/ImageView.cpp
+++ src/lib/Elements/ImageView.cpp
@@ -1,3 +1,23 @@
+/*
+
+  Études Audiovisuel - Graphical elements for audiovisual composition.
+  Copyright (C) 2015-2016 Patric Schmitz
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <glbinding/gl/gl.h>
 
 #include <glm/vec2.hpp>
@@ -80,7 +100,7 @@ namespace etudes {
         glUniform1f(registry.getUniform("textured", "alpha"), alpha);
 
         Rect area(0, 0, image->getWidth(), image->getHeight());
-        area = area.maximizedTo(context.getViewport2D(), Rect::CROP);
+        area = area.maximizedTo(context.getViewport2D(), Rect::STRETCH);
 
         glm::mat4 model(
             area.getWidth(),  0, 0, 0,
--- src/lib/Graphics/ShaderRegistry.cpp
+++ src/lib/Graphics/ShaderRegistry.cpp
@@ -1,7 +1,7 @@
 /*
 
-  Études Audiovisuel - graphical elements for audiovisual composition
-  Copyright (C) 2015 Patric Schmitz, Claudio Cabral
+  Études Audiovisuel - Graphical elements for audiovisual composition.
+  Copyright (C) 2015-2016 Patric Schmitz
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
